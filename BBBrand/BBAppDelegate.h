584c1102ec44647ed703c13f9682d43d8371363d
Hahn.Chan <hahn.chan@corp.globalmarket.com>
2014-07-15 10:53:08 +0800

    init commite

---
+++ BBBrand/BBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BBAppDelegate.h
+//  BBBrand
+//
+//  Created by Bloodshed on 7/15/14.
+//  Copyright (c) 2014 Hahn.Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BBBrand/BBBrandViewController.h
@@ -0,0 +1,13 @@
+//
+//  BBBrandViewController.h
+//  BBBrand
+//
+//  Created by Bloodshed on 7/15/14.
+//  Copyright (c) 2014 Hahn.Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBBrandViewController : UIViewController
+
+@end
